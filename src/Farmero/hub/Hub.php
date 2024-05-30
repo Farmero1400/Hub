3a972225ef8e2af8417c77223faa5543c37a81e0
Farmero1400 <145964153+Farmero1400@users.noreply.github.com>
2024-05-30 00:16:44 -0700

    Update Hub.php

--- src/Farmero/hub/Hub.php
+++ src/Farmero/hub/Hub.php
@@ -6,18 +6,23 @@ namespace Farmero\hub;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
+use pocketmine\world\Position;
 
 use Farmero\hub\Command\HubCommand;
+use Farmero\hub\Command\SetHubCommand;
 
 class Hub extends PluginBase
 {
     private static Hub $main;
+    private ?Position $hubLocation = null;
 
     public function onEnable(): void
     {
         self::$main = $this;
         $this->saveDefaultConfig();
+        
         $this->getServer()->getCommandMap()->register("Hub", new HubCommand());
+        $this->getServer()->getCommandMap()->register("SetHub", new SetHubCommand());
     }
 
     public static function getConfigReplace(string $path, array $replace = [], array $replacer = []): string
@@ -35,5 +40,21 @@ class Hub extends PluginBase
     public static function getInstance(): Hub
     {
         return self::$main;
+    }
+
+    public function loadHubLocation(): void
+    {
+        $hubData = $this->getConfigValue("hub");
+        if ($hubData !== false) {
+            $world = $this->getServer()->getWorldManager()->getWorldByName($hubData["world"]);
+            if ($world !== null) {
+                $this->hubLocation = new Position($hubData["x"], $hubData["y"], $hubData["z"], $world);
+            }
+        }
+    }
+
+    public function getHubLocation(): ?Position
+    {
+        return $this->hubLocation;
     }
 }
