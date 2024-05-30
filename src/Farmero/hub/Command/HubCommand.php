7d3ad157e9a3718b40cc81207b325a60c1c35ca0
Farmero1400 <145964153+Farmero1400@users.noreply.github.com>
2024-05-30 00:19:54 -0700

    Update HubCommand.php

--- src/Farmero/hub/Command/HubCommand.php
+++ src/Farmero/hub/Command/HubCommand.php
@@ -6,12 +6,10 @@ namespace Farmero\hub\Command;
 
 use pocketmine\entity\effect\EffectInstance;
 use pocketmine\entity\effect\VanillaEffects;
-use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
-
 use Farmero\hub\Hub;
-
 use Farmero\hub\Task\HubTask;
 
 class HubCommand extends Command
@@ -20,24 +18,41 @@ class HubCommand extends Command
     {
         $command = explode(":", Hub::getConfigValue("command"));
         parent::__construct($command[0]);
-        if (isset($command[1])) $this->setDescription($command[1]);
+        if (isset($command[1])) {
+            $this->setDescription($command[1]);
+        }
         $this->setAliases(Hub::getConfigValue("command_aliases"));
         $this->setPermission("hub.cmd");
     }
 
-    public function execute(CommandSender $sender, string $commandLabel, array $args)
+    public function execute(CommandSender $sender, string $commandLabel, array $args): bool
     {
-        if ($sender instanceof Player) {
-            $command = explode(":", "command");
-            if ((isset($command[2])) and !($sender->hasPermission($command[2]))) return;
+        if (!$sender instanceof Player) {
+            $sender->sendMessage("This command can only be used in-game.");
+            return false;
+        }
 
-            if ($sender->hasPermission("hub.cmd")) {
-                $sender->teleport($sender->getWorld()->getSafeSpawn());
-                $sender->sendTip(Hub::getConfigReplace("teleportation"));
-            } else {
-                $sender->getEffects()->add(new EffectInstance(VanillaEffects::BLINDNESS(), 20 * (Hub::getConfigValue("delay") + 2), 10));
-                new HubTask($sender);
-            }
+        if (!$this->testPermission($sender)) {
+            return false;
+        }
+
+        if ($sender->hasPermission("hub.cmd")) {
+            $this->teleportToHub($sender);
+        } else {
+            $sender->getEffects()->add(new EffectInstance(VanillaEffects::BLINDNESS(), 20 * (Hub::getConfigValue("delay") + 2), 10));
+            new HubTask($sender);
+        }
+        return true;
+    }
+
+    private function teleportToHub(Player $player): void
+    {
+        $hubLocation = Hub::getInstance()->getHubLocation();
+        if ($hubLocation !== null) {
+            $player->teleport($hubLocation);
+            $player->sendTip(Hub::getConfigReplace("teleportation"));
+        } else {
+            $player->sendMessage(Hub::getConfigReplace("no_hub_set"));
         }
     }
 }
